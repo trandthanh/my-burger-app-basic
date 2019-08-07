d0bd8d39b52d61da4caffa718d1c111513d624b7
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-08-07 11:11:29 +0200

    created layout component, implemented the burger builder component, burger ingredient dynamic, prop type validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
     );
   };
 };
